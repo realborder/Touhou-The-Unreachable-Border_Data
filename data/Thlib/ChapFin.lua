6c76851b43675012a430fd2baf3dd54cb9470815
suigintou <1309287625@qq.com>
2019-03-21 17:18:38 +0800

    chapFin重置
    初始参数isInboss决定是否结算奖残；新增全局函数ClearAllEnemyAndBullet()，用于删除所有敌机和子弹。

--- data/Thlib/ChapFin.lua
+++ data/Thlib/ChapFin.lua
@@ -1,53 +1,57 @@
 LoadImageFromFile('chapFin','THlib\\chapFin.png')
 --用于结算点的obj，奖励chapter分数，结算残机和符卡，以及重置计数
-ChapFin=Class(_object)
-ChapFin.init=function(self,_x,_y,_)
-    self.x,self.y=0,75
+ChapFin=Class(object)
+function ChapFin:init(isInboss)
+	self.x=0
+	self.y=75
 	self.img='chapFin'
 	self.layer=LAYER_TOP
 	self.group=GROUP_GHOST
+	self.hscale=1
+	self.vscale=1
 	self.hide=false
 	self.bound=false
-	self.navi=false
-	self.hp=10
-	self.maxhp=10
 	self.colli=false
-	self._blend,self._a,self._r,self._g,self._b='',255,255,255,255
-	--重置梦现指针
-	DR_Pin.reset()
-	
-	--kill所有杂鱼
-	for _,unit in ObjList(GROUP_ENEMY) do
-		if unit~=_boss then
-			unit.drop=false
-			_kill(unit,true)
-		else
-		end
-	end    
-	_clear_bullet(false,false)
-	
-	task.New(self,function()
-        --动画效果
-		do local s,_d_s=(1),(-0.5/29) local trans,_d_trans=(0),(255/29) for _=1,30 do
-            self.hscale=s
-            self.vscale=s
-            self._a=trans
-            task._Wait(1)
-        s=s+_d_s trans=trans+_d_trans end end
+	self.flag=isInboss
+end
+
+function ChapFin:frame()
+	if self.timer==1 then
+		DR_Pin.reset()
+		ClearAllEnemyAndBullet()
 		--残机结算
-		if lstg.var.chip>=100 and (not IsValid(_boss)) then
+		if lstg.var.chip>=100 and not self.flag then
 			lstg.var.lifeleft,lstg.var.chip=lstg.var.lifeleft+math.modf(lstg.var.chip),lstg.var.chip%100--这里的modf会返回参数的整数和小数部分，我就当fix()用了
 			PlaySound('extend',0.5)
 			New(hinter,'hint.extend',0.6,0,112,15,120)
 		end
 		--符卡结算
-		if lstg.var.bombchip>=100 and (not IsValid(_boss)) then
+		if lstg.var.bombchip>=100 and not self.flag then
 			lstg.var.bomb,lstg.var.bombchip=lstg.var.bomb+math.modf(lstg.var.bombchip),lstg.var.bombchip%100
 			if lstg.var.bomb==3 then lstg.var.bombchip=0 end
 			PlaySound('cardget',0.8)
 		end 
-		task._Wait(120)
-		_del(self,true)
-    end)
-	
+	end
+	if self.timer==120 then Del(self) end
+end
+
+function ChapFin:render()
+	if self.timer<=30 then
+		self.hscale=self.hscale-1/60
+		self.vscale=self.vscale-1/60
+		SetImageState('chapFin','',Color(self.timer*255/30),255,255,255)
+	end
+	Render('chapFin',self.x,self.y,0,self.hscale)
+end
+
+function ClearAllEnemyAndBullet()
+	for _,unit in ObjList(GROUP_ENEMY) do
+		if unit~=_boss then
+			unit.drop=false
+			Kill(unit)
+		end
+	end
+	for _,unit in ObjList(GROUP_ENEMY_BULLET) do
+		Kill(unit)
+	end
 end
