a97167836816da45631e423a82334ddc45c9f1cc
Janrilw <janrilw@qq.com>
2019-05-26 16:03:03 +0800

    换标题曲，以及其循环节点

--- game/Thlib/music/music.lua
+++ game/Thlib/music/music.lua
@@ -1,4 +1,4 @@
 music_list={
-	menu={'THlib\\music\\luastg 0.08.540 - 1.27.800.ogg',87.8,79.26},
+	menu={'THlib\\music\\title.ogg',0x2D6BEE/44100,0x31E85E/44100},
 	spellcard={'THlib\\music\\spellcard.ogg',75,0xc36e80/44100/4},
 }
