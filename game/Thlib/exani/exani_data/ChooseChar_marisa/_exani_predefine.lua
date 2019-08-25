1b1d85de1468a87ae1826e5056f477220296edc3
Janrilw <janrilw@qq.com>
2019-08-25 19:38:39 +0800

    predefine更新

--- game/Thlib/exani/exani_data/ChooseChar_marisa/_exani_predefine.lua
+++ game/Thlib/exani/exani_data/ChooseChar_marisa/_exani_predefine.lua
@@ -8,7 +8,7 @@ _exani_predefine['viewmode']='ui'
 
 _exani_predefine['init']=				{{startf=1,		endf=46},							'keepA'}
 _exani_predefine['keepA']=				{{startf=47,	endf=47,	repeatc=_infinite}}
-_exani_predefine['chooseA']=			{{startf=66,	endf=72,	repeatc=3},				'killA'}
+_exani_predefine['chooseA']=			{{startf=67,	endf=72,	repeatc=3},				'killA'}
 _exani_predefine['killA']=				{{startf=47,	endf=62}}
 _exani_predefine['AtoB']=				{{startf=77,	endf=102},							'keepB'}
 _exani_predefine['BtoA']=				{{startf=102,	endf=77},							'keepA'}
