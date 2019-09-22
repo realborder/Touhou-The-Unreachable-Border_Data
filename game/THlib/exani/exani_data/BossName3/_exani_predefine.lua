f51da5e6cfb5eb6544bfdf386441c0e8e7808f06
Janrilw <janrilw@qq.com>
2019-09-22 20:53:34 +0800

    exani数据上传

---
+++ game/THlib/exani/exani_data/BossName3/_exani_predefine.lua
@@ -0,0 +1,11 @@
+------------------------------------
+--boss名字 通用
+------------------------------------
+local _exani_predefine={}
+local _infinite=-1
+_exani_predefine['layer']=LAYER_TOP+666
+_exani_predefine['viewmode']='world'
+
+_exani_predefine['init']={{startf=1,endf=126}} 
+
+return _exani_predefine
---
+++ game/THlib/exani/exani_data/SpellCardAttack_2/_exani_predefine.lua
@@ -0,0 +1,11 @@
+------------------------------------
+--开卡立绘：通用
+------------------------------------
+local _exani_predefine={}
+local _infinite=-1
+_exani_predefine['layer']=LAYER_PLAYER-1
+_exani_predefine['viewmode']='world'
+
+_exani_predefine['init']={{startf=1,endf=58}}
+
+return _exani_predefine
