76b5ba8c8db4bba3491b2194dbb0667d3204272c
Janrilw <janrilw@qq.com>
2019-10-02 21:11:59 +0800

    解决predefine没有全部上传的问题

--- game/Thlib/exani/exani_data/Title_Menu_LOGO/_exani_predefine.lua
+++
@@ -1,9 +0,0 @@
-local _exani_predefine={}
-local _infinite=-1
-_exani_predefine['layer']=LAYER_TOP
-_exani_predefine['viewmode']='ui'
-
-_exani_predefine['init']={{startf=1,endf=42},{startf=43,endf=43,repeatc=_infinite}}
-_exani_predefine['kill']={{startf=42,endf=1}}
-
-return _exani_predefine
