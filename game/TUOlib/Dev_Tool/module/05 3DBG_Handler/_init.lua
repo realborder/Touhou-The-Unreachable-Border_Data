61bb396e0c6731426553edc8dd1242b05f48898c
Janrilw <janrilw@qq.com>
2019-10-31 21:12:11 +0800

    更新一点3D背景的内容

--- game/TUOlib/Dev_Tool/module/05 3DBG_Handler/_init.lua
+++ game/TUOlib/Dev_Tool/module/05 3DBG_Handler/_init.lua
@@ -6,8 +6,28 @@ end
 local _3dbg_info=TUO_Developer_UI:NewPanel()
 function _3dbg_info:init()
     self.name="3D信息"
-    local title=TUO_Developer_UI:AttachWidget(self,'title')
-    title.text='lstg.view3d信息'
-    local disp=TUO_Developer_UI:AttachWidget(self,'value_displayer')
-	disp.monitoring_value=lstg.view3d
+    TUO_Developer_UI:SetWidgetSlot('world')
+    TDU_New'title'(self).text='lstg.view3d信息'
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='相机位置',v=string.format('%.3f',lstg.view3d.eye[1])..', '..string.format('%.3f',lstg.view3d.eye[2])..', '..string.format('%.3f',lstg.view3d.eye[3])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='相机看向',v=string.format('%.3f',lstg.view3d.at[1])..', '..string.format('%.3f',lstg.view3d.at[2])..', '..string.format('%.3f',lstg.view3d.at[3])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='相机上方',v=string.format('%.3f',lstg.view3d.up[1])..', '..string.format('%.3f',lstg.view3d.up[2])..', '..string.format('%.3f',lstg.view3d.up[3])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='雾气距离',v=string.format('%.3f',lstg.view3d.fog[1])..', '..string.format('%.3f',lstg.view3d.fog[2])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='雾气颜色',v=tostring(lstg.view3d.fog[3])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='z轴裁切 ',v=string.format('%.3f',lstg.view3d.z[1])..', '..string.format('%.3f',lstg.view3d.z[2])}}
+    end
+    TDU_New'value_displayer'(self).monitoring_value=function(widget)
+        return {{name='视野    ',v=string.format('%.3f',lstg.view3d.fovy)}}
+    end
+
 end
--- game/TUOlib/TUO_Background/TUO_Background.lua
+++ game/TUOlib/TUO_Background/TUO_Background.lua
@@ -1,11 +1,16 @@
 tuolib.BGHandler={}
 
 
-
+------------------------------------
+---卸载某个资源
+---@param texname string
+---@return boolean
 function UnLoadImageAndTex(texname)
     local pool=CheckRes('tex',texname)
     if pool then
         RemoveResource(pool,1,texname)
         RemoveResource(pool,2,texname)
+        return true
     end
+    return false
 end 
